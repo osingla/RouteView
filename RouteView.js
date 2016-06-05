59d24d93f817418933324a7afa18a0ba72e02cd3
Olivier <olivier.singla@gmail.com>
2016-06-05 13:52:30 -0400

    Work in progress - various bug fixes

--- RouteView.js
+++ RouteView.js
@@ -940,6 +940,13 @@ define( function( m ) {
 
 				var url = location.host + location.pathname;
 				console.log( "url= [" + url + "]" );
+				if ( url == "rawgit.com/osingla/RouteView/master/RouteView.html" ) {
+					console.log( "Switching to http://streetviewplayer.org/VirtualRide" );
+					var xmlhttp = new XMLHttpRequest();
+					xmlhttp.open("GET", "http://streetviewplayer.org/VirtualRide/", false);
+					xmlhttp.send();
+					return xmlhttp.responseText;
+   				}
 
 				var map_options = {
 					disableDoubleClickZoom: true,
